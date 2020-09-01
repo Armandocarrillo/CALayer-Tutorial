5794f9b3c8d8f79b56f3272918fbfa0027fc9d8b
Armando Carrillo <ingcompuarmando@gmail.com>
2020-08-31 19:49:22 -0500

    CAScroll

--- LayerPlayer/Controllers/CAScrollLayerViewController.swift
+++ LayerPlayer/Controllers/CAScrollLayerViewController.swift
@@ -1,30 +1,4 @@
-/// Copyright (c) 2020 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+
 
 import UIKit
 
@@ -46,9 +20,33 @@ class CAScrollLayerViewController: UIViewController {
 
 // MARK: - IBActions
 extension CAScrollLayerViewController {
+  //when a pan  gesture occurs, you calculate the translation
   @IBAction func panRecognized(_ sender: UIPanGestureRecognizer) {
+    var newPoint = scrollingView.bounds.origin
+    newPoint.x -= sender.translation(in: scrollingView).x
+    newPoint.y -= sender.translation(in: scrollingView).y
+    sender.setTranslation(.zero, in: scrollingView)
+    scrollingViewLayer.scroll(to: newPoint)
+    
+    if sender.state == .ended {
+      UIView.animate(withDuration: 0.3) {
+        self.scrollingViewLayer.scroll(to: CGPoint.zero)
+      }
+    }
   }
 
   @IBAction func scrollingSwitchChanged(_ sender: UISwitch) {
+    //determinate the scrolling direction according to the values
+    
+    switch (horizontalScrollingSwitch.isOn, verticalScrollingSwitch.isOn) {
+    case (true,true):
+      scrollingViewLayer.scrollMode = .both
+    case (true,false):
+      scrollingViewLayer.scrollMode = .horizontally
+    case (false,true):
+      scrollingViewLayer.scrollMode = .vertically
+    default:
+      scrollingViewLayer.scrollMode = .none
+    }
   }
 }
