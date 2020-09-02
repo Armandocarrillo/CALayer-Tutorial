f1bf638249bf7e9712adecc8f91c983a6f27ccbb
Armando Carrillo <ingcompuarmando@gmail.com>
2020-09-02 14:28:09 -0500

    CAEmitterLayer

--- LayerPlayer/Controllers/CAEmitterLayerViewController.swift
+++ LayerPlayer/Controllers/CAEmitterLayerViewController.swift
@@ -1,30 +1,4 @@
-/// Copyright (c) 2020 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+
 
 import UIKit
 
@@ -41,18 +15,64 @@ class CAEmitterLayerViewController: UIViewController {
   }
 
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//    if segue.identifier == "DisplayEmitterControls" {
-//      (segue.destination as? CAEmitterLayerControlsViewController)?.emitterLayerViewController = self
-//    }
+   if segue.identifier == "DisplayEmitterControls" {
+     (segue.destination as? CAEmitterLayerControlsViewController)?.emitterLayerViewController = self
+   }
   }
 }
 
 // MARK: - Layer setup
 extension CAEmitterLayerViewController {
   func setUpEmitterLayer() {
+    
+    //reset the layer
+    resetEmitterCells()
+    emitterLayer.frame = viewForEmitterLayer.bounds
+    viewForEmitterLayer.layer.addSublayer(emitterLayer)
+    // random number generator
+    emitterLayer.seed = UInt32(Date().timeIntervalSince1970)
+    //to set emitter position
+    emitterLayer.emitterPosition = CGPoint(x: viewForEmitterLayer.bounds.midX * 1.5, y: viewForEmitterLayer.bounds.midY)
+    
+    emitterLayer.renderMode = .additive
+    
   }
 
   func setUpEmitterCell() {
+    //to set image that is available in the layer player
+    emitterCell.contents = UIImage(named: "smallStar")?.cgImage
+    // to set initial velocity
+    emitterCell.velocity = 50.0
+    emitterCell.velocityRange = 500.0
+    
+    emitterCell.color = UIColor.black.cgColor
+    //select color ranges
+    emitterCell.redRange = 1.0
+    emitterCell.greenRange = 1.0
+    emitterCell.blueRange = 1.0
+    emitterCell.alphaRange = 0.0
+    emitterCell.redSpeed = 0.0
+    emitterCell.greenSpeed = 0.0
+    emitterCell.blueSpeed = 0.0
+    emitterCell.alphaSpeed = -0.5
+    emitterCell.scaleSpeed = 0.1
+    //
+    let zeroDegreesInRadians = degreesToRadians(0.0)
+    emitterCell.spin = degreesToRadians(130.0)
+    emitterCell.spinRange = zeroDegreesInRadians
+    emitterCell.emissionLatitude = zeroDegreesInRadians
+    emitterCell.emissionLongitude = zeroDegreesInRadians
+    emitterCell.emissionRange = degreesToRadians(360.0)
+    //define the lifetime 1 second
+    emitterCell.lifetime = 1.0
+    emitterCell.birthRate = 250.0
+    //affects the visual angle
+    emitterCell.xAcceleration = -800
+    emitterCell.yAcceleration = 1000
+    
+    
+    
+    
   }
 
   func resetEmitterCells() {
